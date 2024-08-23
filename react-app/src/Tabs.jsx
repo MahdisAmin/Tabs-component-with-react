a43f2f2b3499b6ef32a353d829b91b79c1e9a027
Mahdis Amin <mahdis.amin95@gmail.com>
2024-08-23 17:04:08 +0330

    tabs and tap files added and useState has been  called

---
+++ react-app/src/Tabs.jsx
@@ -0,0 +1,13 @@
+
+import React,{ useState } from 'react'
+import Tap from './Tap'
+
+function Tabs() {
+    const[activeTap,setAcriveTab]=useState(1)
+  return (
+      <>
+      </>
+  )
+}
+
+export default Tabs
---
+++ react-app/src/Tap.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Tap({label,onClick,isActive}) {
+  return (
+      <button> {label}</button>
+  )
+}
+
+export default Tap
