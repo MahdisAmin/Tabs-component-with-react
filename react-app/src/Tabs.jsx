e5e4bae18ab1d4df8a7197b6f67d6e0b0ae9c49e
Mahdis Amin <mahdis.amin95@gmail.com>
2024-08-23 21:34:53 +0330

    new style added

--- react-app/src/Tabs.jsx
+++ react-app/src/Tabs.jsx
@@ -1,32 +1,31 @@
-
-import React,{ useState } from 'react'
-import Tap from './Tap'
+import React, { useState } from "react";
+import Tap from "./Tap";
 
 function Tabs() {
-    const [activeTap, setAcriveTab] = useState(1)
-    const tabClickHnadler = (tabIndex) => {
-        setAcriveTab(tabIndex)
-    }
+  const [activeTap, setAcriveTab] = useState(1);
+  const tabClickHnadler = (tabIndex) => {
+    setAcriveTab(tabIndex);
+  };
   return (
     <>
       <div className="tabs">
         <Tap
-          label="Tab1"
+          label="Tab 1"
           onClick={() => tabClickHnadler(1)}
           isActive={activeTap === 1}
         />
         <Tap
-          label="Tab2"
+          label="Tab 2"
           onClick={() => tabClickHnadler(2)}
           isActive={activeTap === 2}
         />
         <Tap
-          label="Tab3"
+          label="Tab 3"
           onClick={() => tabClickHnadler(3)}
           isActive={activeTap === 3}
         />
         <Tap
-          label="Tab4"
+          label="Tab 4"
           onClick={() => tabClickHnadler(4)}
           isActive={activeTap === 4}
         />
@@ -36,15 +35,14 @@ function Tabs() {
         {activeTap === 2 && <div>Content 2</div>}
         {activeTap === 3 && <div>Content 3</div>}
         {activeTap === 4 && <div>Content 4</div>}
-      </div>
-      <div>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut optio,
           magnam iusto dolore error fuga nisi dolor omnis id animi.
         </p>
       </div>
+      <div></div>
     </>
   );
 }
 
-export default Tabs
+export default Tabs;
