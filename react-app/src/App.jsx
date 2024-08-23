8c828307acaae6580891b69fc4787a0310c18fef
Mahdis Amin <mahdis.amin95@gmail.com>
2024-08-23 18:28:12 +0330

    App.jsx updated

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import Tabs from './Tabs'
+
 
 function App() {
  
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <h1>Tabs Component with React</h1>
+      <Tabs/>
+    </div>
   )
 }
 
